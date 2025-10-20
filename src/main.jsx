0d12cef2ab5cffe6ee263ec4c927d6dbcda9e56d
Andrea Tempestini <the8adventure@gmail.com>
2025-10-20 15:57:16 +0100

    fix(ui): hide duplicate orbit in PROFILE via post-mount DOM patch 2025-10-20-1557

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,11 @@ import FuturistToggle from "./components/FuturistToggle.jsx";
 import FuturistLayout from "./futurist/FuturistLayout.jsx";
 function Root(){ const { futurist } = useMode(); return (<>{futurist ? <FuturistLayout/> : <App/>}<FuturistToggle/></>); }
 ReactDOM.createRoot(document.getElementById("root")).render(
+import { hideProfileDiagram } from "./utils/hideProfileDiagram.js";
   <React.StrictMode>
     <ModeProvider>
       <Root />
     </ModeProvider>
   </React.StrictMode>
 );
+
